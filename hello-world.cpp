33a2b8f69c6f030d1eec39701650b10495074fdb
test <test@git.com>
2020-11-21 12:32:35 +0800

    调整格式

--- hello-world.cpp
+++ hello-world.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <string>
 
-std::string say_hello() { return std::string("Hello, CMake world!"); }
+std::string say_hello(){ 
+	return std::string("Hello, CMake world!");
+}
 
 int main() {
   std::cout << say_hello() << std::endl;
